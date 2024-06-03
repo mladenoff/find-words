d2a2ae6d06059614989184d6d4fdf7ef025a3c0a
Isak Mladenoff <imladenoff@gmail.com>
2024-06-03 15:57:12 -0700

    Add tests

---
+++ findWords.test.js
@@ -0,0 +1,64 @@
+const assert = require('assert')
+
+const findWords = require('./findWords.js')
+
+const cases =  [
+  { inputString: "ate",
+    dictionary: ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"],
+    expected: ["ate", "eat", "tea"],
+    description: "Partial match"
+  },
+
+  {
+    inputString: "oogd",
+    dictionary: ["dog", "do", "god", "goo", "go", "good"],
+    expected: ["dog", "do", "god", "goo", "go", "good"],
+    description: "Complete match"
+  },
+  {
+    inputString: "ate",
+    dictionary: ["dog", "do", "god", "goo", "go", "good"],
+    expected: [],
+    description: "No match"
+
+  }
+]
+
+// // Failing cases - uncomment these and comment out the above cases to test
+// const cases =  [
+//   { inputString: "ate",
+//     dictionary: ["ate", "eat", "tea", "dog", "do", "god", "goo", "go", "good"],
+//     expected: ["ate", "eat"],
+//     description: "Partial match"
+//   },
+
+//   {
+//     inputString: "oogd",
+//     dictionary: ["dog", "do", "god", "goo", "go"],
+//     expected: ["dog", "do", "god", "goo", "go", "good"],
+//     description: "Complete match"
+//   },
+//   {
+//     inputString: "ate",
+//     dictionary: ["dog", "do", "god", "goo", "go", "good"],
+//     expected: ["dog"],
+//     description: "No match"
+
+//   }
+// ]
+
+const test = () => {
+  cases.forEach(({ inputString, dictionary, expected, description }) => {
+    const result = findWords(inputString, dictionary)
+
+    try {
+      assert.deepStrictEqual(result, expected)
+      console.log(`Passed: ${description}`)
+    } catch (error) {
+      console.error(`Failed: ${description}`)
+      console.error(`  Expected [${expected}] got [${result}]`)
+    }
+  })
+}
+
+module.exports = test
---
+++ test.js
@@ -0,0 +1,5 @@
+#! /usr/bin/env node
+
+const tests = require('./findWords.test.js')
+
+console.log(tests())
